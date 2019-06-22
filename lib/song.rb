97590ae6e9b30f1dcee2b2f7368b681f0c8a5630
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-22 19:01:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
 class Song 
-  attr_accessor :name
+  attr_accessor :name, :artist
   
   @@all = []
   
   def initialize(name)
     self.name = name 
+    self.artist = artist
+    @@all << self
   end
+  
 end
